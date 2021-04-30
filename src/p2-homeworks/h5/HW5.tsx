d7c1cd2d469794ac169f242c1b7b208347f354a4
Peleka <ego.iriska@gmail.com>
2021-04-30 16:57:38 +0300

    add pop-up menu

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import Header from './Header'
+import React, {useState} from 'react'
+import {Header} from './Header'
 import Routes from './Routes'
 import {HashRouter} from "react-router-dom";
 
 function HW5() {
+    const [menuCollapsed, setMenuCollapsed] = useState<boolean>(false)
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
             <HashRouter>
 
-            <Header/>
+            <Header
+                onChange={() => setMenuCollapsed(!menuCollapsed)}
+                collapsed={menuCollapsed}
+
+            />
 
             <Routes/>
 
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -3,27 +3,32 @@ import {NavLink} from 'react-router-dom'
 import {PATH} from './Routes'
 import s from './Header.module.css'
 
-function Header() {
+type HeaderType = {
+    onChange: () => void
+    collapsed: boolean
+}
+
+export const Header = (props: HeaderType) => {
     return (
-        <div>
-            <button>ClickMe</button>
-            <div>
-                <div className={s.item}>
-                    <NavLink to={PATH.PRE_JUNIOR} activeClassName={s.active}>pre_junior</NavLink>
-                </div>
-
-                <div className={s.item}>
-                    <NavLink to={PATH.JUNIOR} activeClassName={s.active}>junior</NavLink>
-                </div>
-                <div className={s.item}>
-                    <NavLink to={PATH.JUNIOR_PLUS} activeClassName={s.active}>junior +</NavLink>
-                </div>
-
-            </div>
-
+        <div className={s.wrapper}>
+            <button onClick={props.onChange}>ClickMe</button>
+            {!props.collapsed && <MenuLinks/>}
         </div>
-
     )
 }
 
-export default Header
+function MenuLinks() {
+    return (
+        <div className={s.wrapper}>
+            <div className={s.item}>
+                <NavLink to={PATH.PRE_JUNIOR} activeClassName={s.active}>pre_junior</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={PATH.JUNIOR} activeClassName={s.active}>junior</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={PATH.JUNIOR_PLUS} activeClassName={s.active}>junior +</NavLink>
+            </div>
+        </div>
+    )
+}
