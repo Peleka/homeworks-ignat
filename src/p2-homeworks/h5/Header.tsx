494610acfd2335a0eaa1db4f4e7a42cb87c13bb4
Peleka <ego.iriska@gmail.com>
2021-04-30 16:30:01 +0300

    hm05 add navlinks

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import {PATH} from './Routes'
+import s from './Header.module.css'
 
 function Header() {
     return (
         <div>
-            // add NavLinks
+            <button>ClickMe</button>
+            <div>
+                <div className={s.item}>
+                    <NavLink to={PATH.PRE_JUNIOR} activeClassName={s.active}>pre_junior</NavLink>
+                </div>
+
+                <div className={s.item}>
+                    <NavLink to={PATH.JUNIOR} activeClassName={s.active}>junior</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to={PATH.JUNIOR_PLUS} activeClassName={s.active}>junior +</NavLink>
+                </div>
+
+            </div>
 
         </div>
+
     )
 }
 
