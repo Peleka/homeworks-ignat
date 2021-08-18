dfc2809e76b78bb1ff0ce9b3a9c02891d07f8ce0
Peleka <ego.iriska@gmail.com>
2021-08-18 16:17:13 +0300

    HW11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -6,23 +6,33 @@ function HW11() {
     const [value1, setValue1] = useState(0);
     const [value2, setValue2] = useState(100);
 
+    const handleChange = (event: any, newValue: number | number[]) => {
+        if (newValue instanceof Array) {
+            setValue1(newValue[0])
+            setValue2(newValue[1])
+        }
+    }
+
     return (
         <div>
             <hr/>
             homeworks 11
 
             {/*should work (должно работать)*/}
-            <div>
+            <div style={{display: 'flex', marginLeft: '10px', justifyContent: 'space-between', width: '142px'}}>
                 <span>{value1}</span>
                 <SuperRange
+                    value={value1}
+                    onChangeRange={setValue1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
-            <div>
+            <div style={{display: 'flex', marginLeft: '10px', justifyContent: 'space-between', width: '200px'}}>
                 <span>{value1}</span>
                 <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
+                    value={[value1, value2]}
+                    onChangeRange={handleChange}
                 />
                 <span>{value2}</span>
             </div>
